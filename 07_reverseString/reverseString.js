207b0fa3a88fcea65c409ec8734cd7afe5b4b92b
bigmaniac <1matik.m@gmail.com>
2025-05-08 22:13:23 +0200

    Do excercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const split = string.split("");
+    const reversed = split.reverse();
+    const joined = reversed.join("")
+    return joined;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/* TO LEARN - arrow functions
+
+const reverseString = (string) => string.split("").reverse().join("");
+
+*/
