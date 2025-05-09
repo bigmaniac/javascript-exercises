6236b432de0fd877ab8a56f0bef7db23cddb04b9
bigmaniac <1matik.m@gmail.com>
2025-05-09 23:40:03 +0200

    Done excercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    const arrayToSum = Array.from([a, b]).sort();
+    
+    if (arrayToSum.some(item => item < 0)) {
+      return 'ERROR';
+    }
+    
+    if (arrayToSum.some(item => typeof item === "number" && !Number.isInteger(item))) {
+      return 'ERROR';
+    }
+    
+    if (arrayToSum.some(item => typeof item === "string" || Array.isArray(item))) {
+      return 'ERROR';
+    }
+    
+    const result = function(sumValuesInArrayRange) {
+      let sumRange = 0;
+      for (let i = sumValuesInArrayRange[0]; i <= sumValuesInArrayRange[1]; i++) {
+        sumRange += i;
+      }
+      return sumRange;
+    }
+    return result(arrayToSum);
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
