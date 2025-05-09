855dd583e7b9fa02e7f05c2bc61c54f6e6b4cb5d
bigmaniac <1matik.m@gmail.com>
2025-05-09 19:55:30 +0200

    Done 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...itemsToRemove) {
+    const filteredArray = array.filter((item) => !itemsToRemove.includes(item));
+    return filteredArray;
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;
