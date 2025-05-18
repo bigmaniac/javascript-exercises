1ac974044a07d978bf1ded6b4385010557984d6f
bigmaniac <1matik.m@gmail.com>
2025-05-18 22:36:43 +0200

    Do exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(term1, term2) {
+  const sum = term1 + term2;
+  return sum;
 };
 
-const subtract = function() {
-	
+add(0, 0)
+
+const subtract = function(term1, term2) {
+  const difference = term1 - term2;
+  return difference; 
 };
 
-const sum = function() {
-	
+const sum = function(object) {
+  let result = object.reduce((sum, current) => sum + current, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(object) {
+  let result = object.reduce((multiply, current) => multiply * current);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let result = base ** power;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) {
+    return 1
+  }
+  let arr = Array.from({length: n}, (v, i) => i);
+  arr = arr.map(item => item + 1);
+  let result = arr.reduce((sum, current) => sum * current);
+  return result;
 };
 
 // Do not edit below this line
