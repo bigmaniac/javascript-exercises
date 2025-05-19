248bf39bf5ff1b2c26abf3df2e63597c04f38d72
bigmaniac <1matik.m@gmail.com>
2025-05-19 19:21:10 +0200

    Done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function(phrase) {
+    phrase = phrase.toLowerCase().replace(/\W/g, '');
+    phraseReverse = Array.from(phrase).reverse().join("");
+    if (phrase === phraseReverse) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
